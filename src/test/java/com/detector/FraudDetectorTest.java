4f556f21f998b7ae1fa63650e6928f5754d3e89d
Nadejda <nadyash295923@gmail.com>
2024-09-02 18:44:51 +0300

    feat: add test for list of fraud names

---
+++ src/test/java/com/detector/FraudDetectorTest.java
@@ -0,0 +1,25 @@
+package test.java.com.detector;
+
+import main.java.com.detector.model.FraudDetector;
+import main.java.com.detector.model.Trader;
+import main.java.com.detector.model.Transaction;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FraudDetectorTest {
+    @Test
+    public void findFraudByTraderNameReturnTrue(){
+        FraudDetector fraudDetector = new FraudDetector();
+        fraudDetector.addFraudNames("anonymous");
+        Transaction transaction = new Transaction(new Trader("anonymous", "moskow"), 1500);
+        assertEquals(true, fraudDetector.isFraud(transaction));
+    }
+    @Test
+    public void findFraudByTraderNameReturnFalse(){
+        FraudDetector fraudDetector = new FraudDetector();
+        fraudDetector.addFraudNames("anonymous");
+        Transaction transaction = new Transaction(new Trader("notAnonymous", "moskow"), 1500);
+        assertEquals(false, fraudDetector.isFraud(transaction));
+    }
+}
